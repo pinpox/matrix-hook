e5cf48432332c284e279920d1243987a887ebf52
Pablo Ovelleiro Corral <mail@pablo.tools>
2021-12-01 19:55:50 +0100

    Add template to package

--- flake.nix
+++ flake.nix
@@ -30,7 +30,14 @@
             vendorSha256 =
               "sha256-185Wz9IpJRBmunl+KGj/iy37YeszbT3UYzyk9V994oQ=";
             subPackages = [ "." ];
+            installPhase = ''
+              mkdir -p $out/bin
+              cp $GOPATH/bin/matrix-hook $out/bin/matrix-hook
+              cp message.html.tmpl $out/bin/message.html.tmpl
+            '';
 
+            # mkdir -p $out/bin
+            # mv matrix-hook $out/bin/matrix-hook
             meta = with pkgs.lib; {
               description = "Relay prometheus alerts as matrix messages";
               homepage = "https://github.com/pinpox/matrix-hook";
