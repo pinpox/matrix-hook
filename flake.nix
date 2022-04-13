f15f81084f5ea30bec1b36ac6d81ad0ce46b98e2
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-04-13 10:22:37 +0200

    fix packages source path

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = {  nixpkgs, flake-utils, ... }:
 
     {
 
--- pkgs/matrix-hook.nix
+++ pkgs/matrix-hook.nix
@@ -1,26 +1,25 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.buildGoModule rec {
 
   pname = "matrix-hook";
   version = "1.0.0";
 
-  src = ./.;
-  vendorSha256 =
-    "sha256-185Wz9IpJRBmunl+KGj/iy37YeszbT3UYzyk9V994oQ=";
-    subPackages = [ "." ];
-    installPhase = ''
-              mkdir -p $out/bin
-              cp $GOPATH/bin/matrix-hook $out/bin/matrix-hook
-              cp message.html.tmpl $out/bin/message.html.tmpl
-    '';
+  src = ./..;
+  vendorSha256 = "sha256-185Wz9IpJRBmunl+KGj/iy37YeszbT3UYzyk9V994oQ=";
+  subPackages = [ "." ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $GOPATH/bin/matrix-hook $out/bin/matrix-hook
+    cp message.html.tmpl $out/bin/message.html.tmpl
+  '';
 
-    # mkdir -p $out/bin
-    # mv matrix-hook $out/bin/matrix-hook
-    meta = with pkgs.lib; {
-      description = "Relay prometheus alerts as matrix messages";
-      homepage = "https://github.com/pinpox/matrix-hook";
-      license = licenses.gpl3;
-      maintainers = with maintainers; [ pinpox ];
-    };
+  # mkdir -p $out/bin
+  # mv matrix-hook $out/bin/matrix-hook
+  meta = with pkgs.lib; {
+    description = "Relay prometheus alerts as matrix messages";
+    homepage = "https://github.com/pinpox/matrix-hook";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ pinpox ];
   };
+}
